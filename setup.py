f84f84f284334764333dc1d1049115263f43fb86
Simon de Haan <simon@praekeltfoundation.org>
2013-06-16 21:26:38 +0200

    attempting to fix setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
 
 
-def listify(filename):
+def requirements(filename):
     return filter(None, open(filename, 'r').read().split('\n'))
+
+
+def install_requires(filename):
+    return [r for r in requirements(filename) if not r.startswith('http')]
+
+
+def dependency_links(filename):
+    return [r for r in requirements(filename) if r.startswith('http')]
 
 setup(
     name="txgsm",
@@ -23,7 +31,8 @@ setup(
         'txgsm.etc': ['txgsm/etc/*']
     },
     include_package_data=True,
-    install_requires=listify('requirements.pip'),
+    install_requires=install_requires('requirements.pip'),
+    dependency_links=dependency_links('requirements.pip'),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
