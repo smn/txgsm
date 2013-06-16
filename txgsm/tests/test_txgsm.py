85b67af6aa25f2ba4daf9756020feb3dbf1dc3cc
Simon de Haan <simon@praekeltfoundation.org>
2013-06-16 09:35:36 +0200

    test for configureModem

--- txgsm/tests/test_txgsm.py
+++ txgsm/tests/test_txgsm.py
@@ -14,7 +14,7 @@ class TxGSMTextCase(TestCase):
         self.protocol.makeConnection(self.transport)
         self._command_index = 0
 
-    def reply(self, data):
+    def reply(self, data, delimiter=None):
         self.protocol.dataReceived(data)
 
     def get_next_commands(self):
@@ -25,11 +25,27 @@ class TxGSMTextCase(TestCase):
     def assertCommands(self, commands):
         self.assertEqual(commands, self.get_next_commands())
 
+    def expectExchange(self, command, reply):
+        self.assertCommands([command])
+        self.reply(reply)
+
+    @inlineCallbacks
+    def test_configure_modem(self):
+        d = self.protocol.configureModem()
+        self.expectExchange('AT+CMGF=0', 'OK')
+        self.expectExchange('ATE0', 'OK')
+        self.expectExchange('AT+CMEE=1', 'OK')
+        self.expectExchange('AT+WIND=0', 'OK')
+        self.expectExchange('AT+CSMS=1', 'OK')
+        self.expectExchange('AT+CSQ', 'OK')
+        response = yield d
+        self.assertEqual(response, ['OK'])
+
     @inlineCallbacks
     def test_send_sms(self):
         d = self.protocol.sendSMS('+27761234567', 'hello world')
         self.assertCommands(['AT+CMGS=23'])
-        self.reply('> ')
+        self.reply('> ', delimiter='')
         [pdu_payload] = self.get_next_commands()
         self.reply('OK')
         response = yield d
