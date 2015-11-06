b63baadb4128aebcc7bdd3d8819848aa64b936a5
Simon de Haan <simon@praekeltfoundation.org>
2015-11-06 20:31:12 +0000

    housekeeping

--- setup.py
+++ setup.py
@@ -1,37 +1,10 @@
 from setuptools import setup
 
-import re
-
 
 def listify(filename):
     return filter(None, open(filename, 'r').read().split('\n'))
 
-
-_SIMPLE_VERSION_RE = re.compile("(?P<name>.*)-(?P<version>[0-9.]+|dev)$")
-
-
-def parse_requirements(filename):
-    install_requires = []
-    dependency_links = []
-    for requirement in listify(filename):
-        if requirement.startswith("#"):
-            continue
-        if requirement.startswith("-e"):
-            continue
-        if requirement.startswith("https:") or requirement.startswith("http:"):
-            (_, _, name) = requirement.partition('#egg=')
-            ver_match = _SIMPLE_VERSION_RE.match(name)
-            if ver_match:
-                # egg names with versions need to be converted to
-                # an == requirement.
-                name = "%(name)s==%(version)s" % ver_match.groupdict()
-            install_requires.append(name)
-            dependency_links.append(requirement)
-        else:
-            install_requires.append(requirement)
-    return install_requires, dependency_links
-
-install_requires, dependency_links = parse_requirements("requirements.pip")
+install_requires = listify("requirements.txt")
 
 setup(
     name="txgsm",
@@ -52,7 +25,6 @@ setup(
     },
     include_package_data=True,
     install_requires=install_requires,
-    dependency_links=dependency_links,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
