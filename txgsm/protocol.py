088162f64b48ab04b82e6bd16253b8e68b6d675a
Simon de Haan <simon@praekeltfoundation.org>
2015-11-18 20:42:36 +0100

    wip for better handling of sending commands & tracking responses to them. Very early and breaks a lot of things

--- txgsm/protocol.py
+++ txgsm/protocol.py
@@ -5,46 +5,119 @@ from twisted.protocols.basic import LineReceiver
 from twisted.internet.defer import Deferred
 from twisted.python import log
 
+from collections import defaultdict
+
 from .utils import quote
 
 from messaging.sms import SmsSubmit, SmsDeliver
 
 
-class TxGSMProtocol(LineReceiver):
-
+class ATProtocol(LineReceiver):
     CTRL_Z = '\x1a'
     delimiter = '\r\n'
-    verbose = False
+    verbose = True
 
     def __init__(self):
-        # AT switches being purely line oriented and sometimes not
-        # especially when sending multipart SMSs which has a '^> $' prompt
-        # without a '\r\n' that we need to wait for.
-        # As a result using lineReceived() does not always work.
-        self.setRawMode()
-        self.deferreds = []
+        self.command_queue = []
+        self.responses_received = []
+        self.unsolicited_result_callbacks = defaultdict(list)
+        self.solicited_result_queue = defaultdict(list)
         self.buffer = b''
 
-    def log(self, msg):
+    def log(self, *args):
         if self.verbose:
-            log.msg(msg)
+            log.msg(*args)
 
     def connectionMade(self):
-        self.log('Connection made')
+        log.msg('Connection made')
+
+    def register_unsolicited_result_callback(self, response, cb):
+        """
+        Register a callback function that should be called when ``response``
+        is received from the modem when it was not necessarily expected.
+
+        :param str response:
+            The response to be on the lookout for (i.e. ``+CRING``)
+        :param function cb:
+            The callback function to call when ``response`` is seen.
+        """
+        self.unsolicited_result_callbacks[response].append(cb)
+
+    def deregister_unsolicited_result_callback(self, response, cb):
+        """
+        Deregister a callback function for a specific response.
+
+        :param str response:
+            The response the callback was originally registered for.
+        :param function cb:
+            The callback function to remove.
+        """
+        self.unsolicited_result_callbacks[response].remove(cb)
 
     def send_command(self, command, expect='OK', timeout=None):
-        self.log('Sending: %r' % (command,))
-        resp = Deferred()
-        resp.addCallback(self.debug)
-        if timeout:
-            reactor.callLater(timeout, resp.cancel)
-        self.deferreds.append((command, expect, resp))
-        self.sendLine(command)
-        return resp
+        """
+        Send a command to the modem. This function returns a deferred that's
+        fired when the value of ``expect`` is received from the modem.
 
-    def debug(self, resp):
-        self.log('Received: %r' % (resp,))
-        return resp
+        :param str expect:
+            The AT response to be expecting back which signals the response
+            expected for this command. This will result in the returned
+            Deferred being fired with response as the payload.
+        :param float timeout:
+            How many seconds to wait for a response before cancellined the
+            Deferred. This will result in the Deferred's ``errback`` being
+            fired with a ``CancelledError``.
+        """
+        self.log('Sending: %r' % (command,))
+        d = Deferred()
+        d.addCallback(self.debug)
+        if timeout:
+            reactor.callLater(timeout, d.cancel)
+        self.solicit_result(command, expect, d)
+        self.sendLine(command)
+        return d
+
+    def debug(self, arg):
+        log.msg('DEBUG: %r' % arg)
+        return arg
+
+    def solicit_result(self, command, expect, deferred):
+        self.solicited_result_queue[expect].append((command, deferred))
+
+    def lineReceived(self, line):
+        if line == 'OK':
+            responses_received, self.responses_received = (
+                self.responses_received, [])
+            d = self.command_queue.pop()
+            d.callback(responses_received)
+        elif line == 'ERROR':
+            responses_received, self.responses_received = (
+                self.responses_received, [])
+            d = self.command_queue.pop()
+            d.errback(responses_received)
+        else:
+            self.responses_received.append(line)
+
+    def parse_responses_received(self, responses_received):
+        for line in responses_received:
+            result, _ = line.split(':', 1)
+            expected_handlers = self.solicited_result_queue[result]
+            if expected_handlers:
+                # FIXME: possibility of a race condition here.
+                command, deferred = expected_handlers.pop()
+                deferred.callback({
+                    'command': [command],
+                    'expect': result,
+                    'response': [line],
+                })
+                return
+
+            unexpected_handlers = self.unsolicited_result_callbacks[result]
+            for handler in unexpected_handlers:
+                handler(line)
+
+
+class TxGSMProtocol(ATProtocol):
 
     def next(self, command, expect='OK'):
         def handler(result):
@@ -70,14 +143,25 @@ class TxGSMProtocol(LineReceiver):
         #       in a for-loop easier since we're potentially sending
         #       SMSs bigger than 160 chars.
         d = Deferred()
+        d.addCallback(self.toggle_raw_mode)
         for pdu in sms.to_pdu():
             d.addCallback(self.next(
                 'AT+CMGS=%d' % (pdu.length,),
                 expect='> '))
             d.addCallback(self.next('%s%s' % (pdu.pdu, self.CTRL_Z)))
 
+        d.addCallback(self.toggle_line_mode)
         d.callback([])
+
         return d
+
+    def toggle_raw_mode(self, args):
+        self.setRawMode()
+        return args
+
+    def toggle_line_mode(self, args):
+        self.setLineMode(extra=self.buffer)
+        return args
 
     def dial_ussd_code(self, code):
         return self.send_command('AT+CUSD=1,"%s",15' % (quote(code),),
